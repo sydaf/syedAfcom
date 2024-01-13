526e6bc854088e596f9b982c8d7436df61424caf
Syed Ahmad Fakrullah <syedahmadfakhrullah@gmail.com>
2024-01-13 21:54:09 +0800

    toggle dark mode button

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
-  let dark = true;
+import { ref } from 'vue';
+import DarkModeIcon from './components/svg/darkMode.vue';
+import LightModeIcon from './components/svg/lightMode.vue';
 
-  const toggleDarkMode = () => {
-    dark = !dark;
-  };
+let dark = ref(false);
+
+const toggleDarkMode = () => {
+  dark.value = !dark.value;
+};
 </script>
 
 <template>
-  <button @click="toggleDarkMode">Toggle Dark Mode</button>
-  <h1 class="text-3xl font-bold underline">
-    Dark Mode: {{ dark ? 'Enabled' : 'Disabled' }}
-  </h1>
+  <div class="bg-container max-h-screen" :class="{ 'dark-mode': dark, 'light-mode': !dark }">
+    <div class="max-w-5xl">
+      <div class="mode-toggler flex">
+        <transition name="icon-spin" mode="out-in">
+          <DarkModeIcon v-if="dark" key="dark-mode-icon" />
+          <LightModeIcon v-else key="light-mode-icon" />
+        </transition>
+        <button @click="toggleDarkMode">Toggle {{ dark ? 'Dark' : 'Light' }} Mode</button>
+      </div>
+    </div>
+  </div>
 </template>
