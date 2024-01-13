be6cacf36c13f45172130d33f924bc8144299029
Syed Ahmad Fakrullah <syedahmadfakhrullah@gmail.com>
2024-01-13 19:06:16 +0800

    test vue toggle

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+  let dark = true;
 
+  const toggleDarkMode = () => {
+    dark = !dark;
+  };
 </script>
 
 <template>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <button @click="toggleDarkMode">Toggle Dark Mode</button>
+  <h1 class="text-3xl font-bold underline">
+    Dark Mode: {{ dark ? 'Enabled' : 'Disabled' }}
+  </h1>
 </template>
